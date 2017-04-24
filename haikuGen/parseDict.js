d50404b83f7191d2022656c1163c6986c5e25427
aperez25 <alice.perez25@gmail.com>
2017-04-24 16:28:48 -0700

    updated module structure

--- haikuGen/parseDict.js
+++ haikuGen/parseDict.js
@@ -4,20 +4,20 @@ exports.readFile = function(file){
   return fs.readFileSync(file).toString();
 }
 
-exports.syllCount = function(phrase) {
+var syllCount = function(phrase) {
   var count = 0;
-    if (phrase) var syllCount = phrase.match(/\d/g);
-      if(syllCount) count = syllCount.length;
+    if (phrase) var syllables = phrase.match(/\d/g);
+      if(syllables) count = syllables.length;
   return count;
 }
 
-exports.cmuBySyllCount = function(data){
+var cmuBySyllCount = function(data){
   var syllTable = {};
   var lines = data.split("\n"), lineSplit;
   lines.forEach(function(line) {
     lineSplit = line.split("  ");
     lineSplit[0] = lineSplit[0].replace(/[(\d)]/g, '');
-    var syllables = exports.syllCount(lineSplit[1]);
+    var syllables = syllCount(lineSplit[1]);
     if (syllables < 8) {
       if (syllTable.hasOwnProperty(syllables)) {
         syllTable[syllables].push(lineSplit[0]);
@@ -28,3 +28,7 @@ exports.cmuBySyllCount = function(data){
   return syllTable;
 }
 
+module.exports = {
+  cmuBySyllCount: cmuBySyllCount,
+  syllCount: syllCount
+}
